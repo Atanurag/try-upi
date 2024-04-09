136ba378fa146a0e755e31da6e511afb3f189c3d
Atanurag <anuragt9510@gmail.com>
2024-04-09 11:15:37 +0530

    lo12

--- src/demo.js
+++ src/demo.js
@@ -214,7 +214,7 @@ if (status === 'SUCCESS') {
 }
 
 React.useEffect(()=>{
-  if(localStorage.getItem('po') === 12){
+  if(localStorage.getItem('po') == 12){
     alert('ppopo');
     localStorage.removeItem('po')
   }
