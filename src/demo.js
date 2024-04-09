2d11faeb3ad860ebc0f60039905b6e3fef46118e
Atanurag <anuragt9510@gmail.com>
2024-04-09 11:49:50 +0530

    abanc

--- src/demo.js
+++ src/demo.js
@@ -239,7 +239,7 @@ vsbsjn&orgid=00000&mid=1234&msid=3432&mtid=1212 */}
 }
 
 
-}><a href="upi://pay?pa=shivaytiwari951@oksbi&pn=YourName&tr=780ttryr345ms&tn=Paymentservices&am=1&cu=INR&url=https://try-upi-25a5.vercel.app/gs">Pay with UPI</a></button>
+}><a href="upi://pay?pa=shivaytiwari951@oksbi&pn=YourName&tr=780ttryr345ms&tn=Paymentservices&am=1&cu=INR&url=https://try-upi-25a5.vercel.app/gs">Pay with UPI 1 inr</a></button>
   {/* <a href="upi://pay?pa=BHARATPE.90070065432@fbpe&pn=Verified Merchant&mc=0000&tr=hello78340&tn=Pay to BharatPe Merchant&am=1&cu=INR">payment</a>
 
 ab
