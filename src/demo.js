7cd6f7e697aad196b498b5557c68184047f5e31f
Atanurag <anuragt9510@gmail.com>
2024-05-10 23:39:54 +0530

    d

--- src/demo.js
+++ src/demo.js
@@ -291,62 +291,32 @@ async function displayRazorpay() {
   // Getting the order details back
   const { amount, id: order_id, currency } = js;
 
-  const options = {
-      key: "rzp_test_LvLqoT1Xo29wnc", // Enter the Key ID generated from the Dashboard
-      amount: amount.toString(),
-      currency: currency,
-      name: "Soumya Corp.",
-      description: "Test Transaction",
-      order_id: order_id,
+  var options = {
+    "key": "rzp_test_LvLqoT1Xo29wnc", // Enter the Key ID generated from the Dashboard
+    "amount": "50000", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+    "currency": "INR",
+    "name": "Acme Corp", //your business name
+    "description": "Test Transaction",
+    "image": "https://example.com/your_logo",
+    "order_id":  order_id, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+    "callback_url": "https://eneqd3r9zrjok.x.pipedream.net/",
+    "prefill": { //We recommend using the prefill parameter to auto-fill customer's contact information especially their phone number
+        "name": "Gaurav Kumar", //your customer's name
+        "email": "gaurav.kumar@example.com",
+        "contact": "9000090000" //Provide the customer's phone number for better conversion rates 
+    },
+    "notes": {
+        "address": "Razorpay Corporate Office"
+    },
+    "theme": {
+        "color": "#3399cc"
+    }
+};
+var rzp1 = new Razorpay(options);
 
-      config: {
-        display: {
-          blocks: {
-            banks: {
-              name: 'All payment methods',
-              instruments: [
-                {
-                  method: 'upi'
-                },
-                {
-                  method: 'card'
-                },
-                {
-                    method: 'wallet'
-                },
-                {
-                    method: 'netbanking'
-                }
-              ],
-            },
-          },
-          sequence: ['block.banks'],
-          preferences: {
-            show_default_blocks: false,
-          },
-        },
-      },
-    
-
-
-
-     
-      prefill: {
-          name: "Soumya Dey",
-          email: "SoumyaDey@example.com",
-          contact: "9999999999",
-      },
-      notes: {
-          address: "Soumya Dey Corporate Office",
-      },
-      theme: {
-          color: "#61dafb",
-      },
-  };
-
-  const paymentObject = new window.Razorpay(options);
-  paymentObject.open();
+rzp1.open();
 }
+
 
   return (
     <>
