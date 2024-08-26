39500fa6d4f8bd67b8e1bc485b2e6ff9eb51ce63
Atanurag <anuragt9510@gmail.com>
2024-08-26 23:06:58 +0530

    66777

--- src/demo.js
+++ src/demo.js
@@ -387,7 +387,7 @@ function onBuyClicked() {
       data: {
         pa: '7875853859@pthdfc',
         pn: 'Anurag Tiwari-1',
-        tr: '5812AeeeB7puDe',// your custom transaction reference ID
+        tr: '58w12AeeeeeB7puDe',// your custom transaction reference ID
         url:'https://17174cc3-e036-41c5-82a6-1ce90c624cd6-00-2oq5i07bzmsdh.pike.repslit.dev:5000',
         mc: '5812', // your merchant category code
       },
